4b2ad506643eb7206b208f22d2e3f17e4cdd6c29
kkmdevel <kkm.devel@gmail.com>
2024-12-06 12:28:37 +0900

    feat: 새로운 test 브랜치 생성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,16 +1,7 @@
 package calculator;
 
-import calculator.controller.CalculatorController;
-import calculator.domain.SumCalculator;
-import calculator.view.ConsoleView;
-
 public class Application {
     public static void main(String[] args) {
-        ConsoleView view = new ConsoleView();
-        SumCalculator calculator = new SumCalculator();
 
-        CalculatorController calculatorController = new CalculatorController(view, calculator);
-
-        calculatorController.run();
     }
 }
