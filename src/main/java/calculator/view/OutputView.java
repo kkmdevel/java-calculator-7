37b39bc99ed4f89af4c32ef8003f9a24503fa296
kkmdevel <kkm.devel@gmail.com>
2024-12-06 12:44:13 +0900

    feat(OutputView): 출력 클래스 생성
    안내문 출력 기능 추가

---
+++ src/main/java/calculator/view/OutputView.java
@@ -0,0 +1,9 @@
+package calculator.view;
+
+public class OutputView {
+
+    public static void printInputPrefix() {
+        System.out.println(OutputMessage.INPUT_PREFIX.getMessage());
+    }
+
+}
